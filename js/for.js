76387adbc3698a6e0fed9a4e750461bb24120ba6
Leandro Sanchez <leasanchez91@gmail.com>
2023-02-27 21:55:02 -0300

    Javascript, condicionales e iteradores

---
+++ js/for.js
@@ -0,0 +1,5 @@
+var xy = 10;
+// for (inicializacion, condicion, incremento/decremento)
+for (xy; xy > 0; xy--) {
+    console.log("El valor de xy es: " + xy); // Sentencia repetitiva
+  }
---
+++ js/tipos_de_variables.js
@@ -0,0 +1,6 @@
+let x; // Definimos la variable
+console.log(x);
+x = 5; // Le asignamos un valor numero a la variable x
+console.log(x);
+x = "John"; // Modificamos el valor de la variable x por un String
+console.log(x);
